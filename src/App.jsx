80704e648cc2cee5efde1a5664086843aacee346
Miky <miky206@gmail.com>
2022-10-20 19:06:58 +0200

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,11 @@ import Formulario from "./components/Formulario"
 import ListadoPacientes from "./components/ListadoPacientes"
 
 function App() {
-  const [ pacientes, setPacientes] = useState([]);
+  const INITIAL = JSON.parse(localStorage.getItem('pacientes')) ?? [];
+  const [ pacientes, setPacientes] = useState(INITIAL);
   const [ paciente, setPaciente] = useState({});
-  useEffect(()=>{
-    const obtenerLS = ()=>{
-      const pacientesLS = JSON.parse(localStorage.getItem('pacientes'))??[];
-      setPacientes(pacientesLS);
-    }
-  },[])
+
+  
   useEffect (()=>{
     localStorage.setItem('pacientes', JSON.stringify(pacientes));
   }, [pacientes])
